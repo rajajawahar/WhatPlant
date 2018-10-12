51fbf13e3e143f63f6a368db7a876777ea2b0ac1
Raja Jawahar <rajajawahar77@gmail.com>
2018-10-12 15:53:44 +0530

    Bug fixs

--- WhatPlant/ViewController.swift
+++ WhatPlant/ViewController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import Vision
+
 
 class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
     
     let imagePicker = UIImagePickerController()
-
+    
     @IBOutlet weak var browsedImageView: UIImageView!
     
     
@@ -21,16 +23,43 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
         imagePicker.allowsEditing = true
         imagePicker.sourceType = .camera
     }
-
+    
     @IBAction func browseCamera(_ sender: UIBarButtonItem) {
         present(imagePicker, animated: true, completion: nil)
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        let userpickedImage = info[UIImagePickerController.InfoKey.originalImage]
-        browsedImageView.image = userpickedImage as? UIImage
+        if let userpickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            
+            guard let convertedCIImage = CIImage(image: userpickedImage) else {
+                fatalError("Cannot convert to CIImage..")
+                
+                
+            }
+            detectImage(seletedImage: convertedCIImage)
+            browsedImageView.image = userpickedImage
+        }
         imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detectImage(seletedImage : CIImage){
         
+        guard let coreMLModel = try? VNCoreMLModel(for: PlantsImageClassifier().model)else {
+            print("Unable to import model")
+            return
+        }
+        let request = VNCoreMLRequest(model: coreMLModel) { (request,error) in
+            let classification = request.results?.first as? VNClassificationObservation
+            self.navigationItem.title = classification?.identifier
+            
+        }
+        let requestOptions:[VNImageOption : Any] = [:]
+        let handler = VNImageRequestHandler(ciImage:seletedImage, options: requestOptions)
+        do{
+            try handler.perform([request])
+        }catch{
+            
+        }
     }
     
 }
